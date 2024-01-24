a55fddeacda31da3ad82f54f3e643f14fb0ea8c0
faisal <faisal.mahboob@hotmail.com>
2024-01-25 02:41:20 +1100

    Initial Commit

--- Data_Pipelines_Generators.py
+++ Data_Pipelines_Generators.py
@@ -23,23 +23,3 @@ funding = (
 total_series_a = sum(funding)
 
 print(f"Total series A fundraising: ${total_series_a}")
-
-
-
-
-
-"""
-import csv
-
-with open('techcrunch.csv') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    line_count = 0
-    for row in csv_reader:
-        if line_count == 0:
-            print(f'Column names are {", ".join(row)}')
-            line_count += 1
-        else:
-            print(f'\t{row[0]} works in the {row[1]} department, and was born in {row[2]}.')
-            line_count += 1
-    print(f'Processed {line_count} lines.')
-"""
